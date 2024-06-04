1c3d63a912711ef25f4472f445109c907cc0c3ee
juansefdz <jsfm81091@gmail.com>
2024-06-04 08:25:50 -0500

    update microservice User

---
+++ src/main/java/com/riwi/filtroSpringBoot/api/dto/response/UserResponse/SurveyResponseInUser.java
@@ -0,0 +1,28 @@
+package com.riwi.filtroSpringBoot.api.dto.response.UserResponse;
+
+import java.time.LocalDate;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SurveyResponseInUser {
+
+    @Schema(description = "ID of the survey") // SWAGGER
+    private int idSurvey;
+    @Schema(description = "title of  the survey") // SWAGGER
+    private String title;
+    @Schema(description = "description of  the survey") // SWAGGER
+    private String description;
+    @Schema(description = "Date of  the survey") // SWAGGER
+    private LocalDate creationDate;
+    @Schema(description = "status of  the survey") // SWAGGER
+    private boolean active;
+    
+}
